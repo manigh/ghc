1c45b041fa1bd60548d1dc7ce6131d7802a75cee
ManiGhahremani <manighahrmani@yahoo.com>
2018-03-05 17:26:43 +0000

    main commencement

--- car.py
+++ car.py
@@ -6,16 +6,16 @@ class Car:
         self.i= i
         self.j= j
         self.listOfDoneRides= []
-        self.schedule = {}
         self.available_time=0
-        self.pos_at_endof_lastride=[0,0]
+        self.pos_at_endof_lastride=[self.i,self.j]
 
 
     #at time t:
     #if is_available(t)==True
-    #get_pos_at_endof_lastride[] based on this:
-    #if we decided that the car can make it based on its distance and t
+    #car.get_pos_at_endof_lastride[], based on this:
     #car do_this_ride: returns[pos_at_endof_lastride [], available_time (int)]
+    #!!! if available_time (int)>T => raise ValueError('ride took too long')
+    #!!! if pos_at_endof_lastride [] != ride.get_drop_off_loc [] => raise ValueError('ride went somewhere else')
 
     def time_of_last_ride(self):
         return self.available_time
@@ -31,13 +31,24 @@ class Car:
         self.j= location_list[1]
         self.pos_at_endof_lastride=[self.i, self.j]
 
+    def distance_to_next_ride(self, ij):
+        myij=get_pos_at_endof_lastride()
+        distance_to_next_ride=0
+        distance_to_next_ride=abs(int(ij[0])-int(myij[0]))+abs(int(ij[1])-int(myij[1]))
+        return int(distance_to_next_ride)
+
     def do_this_ride(self, this_ride, t):
         self.listOfDoneRides.append(this_ride)
-        self.available_time=t+this_ride.duration
+        self.available_time=int(t)+int(this_ride.duration)+int(distance_to_next_ride(this_ride))
         ij=this_ride.get_drop_off_loc()
         set_pos(ij)
-        return [get_pos_at_endof_lastride(),self.available_time]
-
+        ride_done_check=[]
+        ride_done_check=this_ride.do_it(t)
+        if(ride_done_check[0]!=ij):
+            raise ValueError('ride end is not same as car ij after done ride')
+        bonus_awarded=0
+        bonus_awarded=ride_done_check[1]
+        return [get_pos_at_endof_lastride(),self.available_time,bonus_awarded]
 
     def is_available(self,t):
         return (time_of_last_ride() <= t)
--- ride.py
+++ ride.py
@@ -11,22 +11,25 @@ class Ride:
         self.endj= int(endj)
         self.earliest= int(earliest)
         self.latest= int(latest)
-        self.drop_off_time = -1
         self.duration= abs(self.starti - self.endi)+abs(self.startj - self.endj)
         self.is_Done = False
         self.is_possible= True
         self.deadline=self.latest-self.duration
         self.B=int(B)
 
-    #for all cars available at time t:
-    #take car c, c.get_pos_at_endof_lastride[]
 
     #for all rides
     #if !isDone
     #if is_possible at t
 
-    #take car c, c.get_pos_at_endof_lastride[]
-    #if car at ij ,t: time_to_make_itAND_bonus(car_list_index,t)=[_,_]
+    #for all cars
+    #if car.is_available(t)==True
+    #car.get_pos_at_endof_lastride[], based on this:
+    #car do_this_ride: returns[pos_at_endof_lastride [], available_time (int)]
+    #!!! if available_time (int)>T => raise ValueError('ride took too long')
+    #!!! if pos_at_endof_lastride [] != ride.get_drop_off_loc [] => raise ValueError('ride went somewhere else')
+
+    #if car at ij ,t: time_to_make_itAND_bonus(car,t)=[_,_]
     #time_to_make_itAND_bonus[0]=can get to DEADLINE and have this time to spare (int)
     #time_to_make_itAND_bonus[1]=can get to BONUS and have this time to spare (int)
 
@@ -41,19 +44,21 @@ class Ride:
         #if time_to_make_itAND_bonus[1]<0 car has MISSED BONUS
 
     #if we decide to do it
-    #use ride_list_index-> do_this_ride(ride_list_index,t)-> update car
-    #do_it to return [drop_off_time t, get_location_to [ij], bonus_at_time int]
+    #this_car.do_this_ride.do_this_ride()
+    #updates this_car.listOfDoneRides
+    #updates this_car.available_time set to t+this_ride.duration+this_car.distance_to_next_ride(this_ride)
+    #updates this_car.ij -> set to get_drop_off_loc
+    #also calls do_it(this_car,t) returns bonus_awarded and updates ride.isDone to True
 
-    def get_drop_off_loc(self):
+    def get_pick_up_loc(self):
         return [self.starti, self.startj]
 
-    def get_drop_off_time(self):
-        return self.drop_off_time;
+    def get_drop_off_loc(self):
+        return [self.endi, self.endj]
 
-    def do_it(self,t):
-        self.drop_off_time = t+self.duration
+    def do_it(self, t):
         self.is_Done = True
-        return [self.drop_off_time,get_drop_off_loc(),bonus_at_time(t)]
+        return [get_drop_off_loc(),bonus_at_time(t)]
 
     def is_possible(self, t):
         if (not self.is_possible):
@@ -85,9 +90,9 @@ class Ride:
         return bonus
 
     #this car, at time t, we already have checked if its availble
-    def time_to_make_itAND_bonus(self, thiscar, t):
+    def time_to_make_itAND_bonus(self, this_car, t):
         car_ij=this_car.pos_at_endof_lastride()
-        distance_to=abs(car_ij[0]-self.starti)+abs(car_ij[1]-self.startj)
+        distance_to=this_car.distance_to_next_ride(get_pick_up_loc)
         time_to_make_it=time_left(t)-distance_to
         time_to_make_it_bonus=to-time_left_bonus(t)-distance_to
         return [time_to_make_it,time_to_make_it_bonus]
